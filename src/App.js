ec39a58bb63efdc6f20d04486015d0e7910d6e06
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-19 15:07:08 +0530

    app work

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import HeadSection from './components/HeadSection';
 import FooterSection from './components/FooterSection';
 import Note from './components/Note';
+import Card from './components/Card';
 
 function App() {
-  return <div>
-    <HeadSection/>
-    <FooterSection/>
-    <Note/>
-  </div>;
+  return (
+    <div>
+    <App/>
+    </div>
+  );
 }
 
 export default App;
