5bddb8ff729898b89e674f14fc8d8dc7df974364
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-21 09:35:59 +0530

    map contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ContactCard from './ContactCard';
+import contacts from '../contacts';
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <ContactCard
-        name="Beyonce"
-        image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        email="b@beyonce.com"
-        phone="+123 456 789"
-      />
+      {contacts.map((contact) => (
+        <ContactCard
+          name={contact.name}
+          image={contact.imgURL}
+          email={contact.email}
+          phone={contact.phone}
+        />
+      ))}
     </div>
   );
 }
