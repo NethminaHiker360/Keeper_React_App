5eea96fcd91bfab48c872f101a7292fe79f43f06
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-23 12:37:47 +0530

    clear for emojipedia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,7 @@
 import React from 'react';
-import ContactCard from './ContactCard';
-import contacts from '../contacts';
-import Avatar from './Avatar';
-
-function createCard(contact) {
-  return (
-    <ContactCard
-      key={contact.id}
-      id={contact.id}
-      name={contact.name}
-      image={contact.imgURL}
-      phone={contact.phone}
-      email={contact.email}
-    />
-  );
-}
 
 function App() {
-  return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <Avatar img={contacts[0].imgURL} />
-      {contacts.map(createCard)}
-
-      {/* <ContactCard
-        name={contacts[0].name}
-        image={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      /> */}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
