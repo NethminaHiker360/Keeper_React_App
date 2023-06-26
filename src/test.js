1c3db5e4a9138123e89cd78c40ee2d3c51e5277e
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-26 09:12:43 +0530

    arrow function

--- src/test.js
+++ src/test.js
@@ -1,7 +1,30 @@
-import emojipedia from "./emojipedia";
+var numbers = [3, 56, 2, 48, 5];
 
-var newArray= emojipedia.map(function(emoji){
-    return emoji.meaning.substring(0,100);
-});
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
 
-console.log(newArray);
+var newNumbers=numbers.map(x=>x*2);
+console.log(newNumbers);
+
+
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(function(num) {
+//   return num < 10;
+// });
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//     return accumulator + currentNumber;
+// })
+
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// })
+
+////FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// })
