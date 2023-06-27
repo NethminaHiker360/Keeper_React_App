8aa2396e773b077fedbf96aea2267d1d8749fb91
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-27 20:56:07 +0530

    render condition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
+var isLogginedIn = false;
+
+function renderConditionally() {
+  if (isLogginedIn) {
+    return <h1>Hello</h1>;
+  } else {
+    return (
+      <div className="container">
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return renderConditionally();
 }
 
 export default App;
